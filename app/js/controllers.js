d53b7a247ec98df4f7877cd675d9f9c3f64d5c62
mattkelsey <mattkelseymk@gmail.com>
2016-06-11 23:48:10 -0700

    Add toggleable settings menu

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,5 +10,6 @@ function NoteListFunc($scope) {
     $scope.controlTitleVal;
     $scope.controlContentVal;
     $scope.id;
+    $scope.showSettings;
     $scope.notes = [{"title": "Welcome to Noteular", "content": "Create a note below!", "id": "0"}];
 }
