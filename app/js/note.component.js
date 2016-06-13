3dc3749a1b435c1fa2eb2734fde3fa8ca97eb4bb
mattkelsey <mattkelseymk@gmail.com>
2016-06-12 23:38:08 -0700

    Create component and restore partial functionality

--- app/js/note.component.js
+++ app/js/note.component.js
@@ -1,14 +1,54 @@
 angular.
     module('noteularApp').
     component('note', {
-        template:
-            '<div class="note">' +
-            '<h class="note-title">{{note.title}}</h>' +
-            '<p class="note-content">{{note.content}}</p>' +
-            '</div>',
-        controller: function NoteController() {
-            this.controlTitleVal;
-            this.controlContentVal;
-            this.notes = [{"title": "Welcome to Noteular", "content": "Create a note below!"}];
-        }
+        templateUrl: 'note.component.html',
+        controller: NoteController
     });
+
+
+function NoteController () {
+    var controlTitleVal;
+    var controlContentVal;
+    var id;
+    var showSettings;
+    var hidden;
+    var notes = [{"title": "Welcome to Noteular", "content": "Create a note below!", "id": "0"}];
+
+    var makeNote = function (title, content) {
+        notes.push({'title': title, 'content': content});
+        controlTitleVal = "";
+        controlContentVal = "";
+    }
+    var handleNoteKey = function(event) {
+        if(event.keyCode == 13) {
+            makeNote($scope.controlTitleVal, $scope.controlContentVal);
+        }
+    }
+
+    angular.element(document).on("click", function(e) {
+        var target = angular.element(e.target);
+        var noteBody = target.parent().parent().parent();
+        if (target.hasClass("color-blue")) {
+            noteBody.addClass("bg-color-blue");
+            noteBody.removeClass("bg-color-yellow");
+            noteBody.removeClass("bg-color-orange");
+            noteBody.removeClass("bg-color-red");
+        } else if (target.hasClass("color-yellow")) {
+            noteBody.addClass("bg-color-yellow");
+            noteBody.removeClass("bg-color-blue");
+            noteBody.removeClass("bg-color-orange");
+            noteBody.removeClass("bg-color-red");
+        } else if (target.hasClass("color-orange")) {
+            noteBody.addClass("bg-color-orange");
+            noteBody.removeClass("bg-color-blue");
+            noteBody.removeClass("bg-color-yellow");
+            noteBody.removeClass("bg-color-red");
+        } else if (target.hasClass("color-red")) {
+            noteBody.addClass("bg-color-red");
+            noteBody.removeClass("bg-color-blue");
+            noteBody.removeClass("bg-color-yellow");
+            noteBody.removeClass("bg-color-orange");
+        }
+    })
+}
+
